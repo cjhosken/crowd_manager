aa737b75a360a5fc76e7b7413ea84e3eae759a3e
Christopher Hosken <hoskenchristopher@gmail.com>
2021-07-22 23:44:12 +0700

    update gl

--- gl_view.py
+++ gl_view.py
@@ -38,25 +38,34 @@ def crowdmanager_gl_draw():
             if "point" in node.node_types:
                 if node.GL_POINTS is not None and len(node.GL_POINTS) > 0:
                     for point in node.GL_POINTS:
-                        points.append(point.location)
-                        colors.append(node.GL_COLOR)
+                        tri = make_tri(point.location, node.GL_COLOR)
+                        points += tri[0]
+                        colors += tri[1]
 
             elif "agent" in node.node_types:
                 if node.GL_AGENTS is not None and len(node.GL_AGENTS) > 0:
                     for agent in node.GL_AGENTS:
                         if agent.simulated == True:
-                            points.append(agent.sim[bpy.context.scene.frame_current - agent.sim_start - 1].location)
-                            colors.append(node.GL_COLOR)
+                            tri = make_tri(agent.sim[bpy.context.scene.frame_current - agent.sim_start - 1].location, node.GL_COLOR)
+                            points += tri[0]
+                            colors += tri[1]
                         else:
-                            points.append(agent.sim[0].location)
-                            colors.append(node.GL_COLOR)
+                            tri = make_tri(agent.sim[0].location, node.GL_COLOR)
+                            points += tri[0]
+                            colors += tri[1]
 
 
         batch = batch_for_shader(shader, 'POINTS', {'pos': points, 'color': colors})
+        bgl.glEnable(bgl.GL_DEPTH_TEST)
         bgl.glPointSize(10)
+        bgl.glEnable(bgl.GL_LINE_SMOOTH)
         shader.bind()
         batch.draw(shader)
 
+def make_tri(point, color):
+    points = [point]
+    colors = [color] * len(points)
+    return [points, colors]
 
 def register():
     global CROWDMANAGER_FRAME_HANDLE, CROWDMANAGER_DRAW_HANDLE
